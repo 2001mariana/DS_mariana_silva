4c83d4ee0cbaa025897cb67f96838b80ea8d0063
Mariana Luiza Vitor Alves Silva <mariana.luiza@spcbrasil.org.br>
2022-10-03 15:43:46 -0300

    criacao do componente inputQuant

---
+++ stories/ABinputQuant.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { AbInputQuant, AbInputQuantProps } from '../src'
+
+export default {
+  title: 'componentes/AbInputQuant',
+  component: AbInputQuant,
+  parameters: { actions: { argTypesRegex: '^on.*' } },
+} as ComponentMeta<typeof AbInputQuant>;
+
+const Template: ComponentStory<typeof AbInputQuant> = (args) => <AbInputQuant {...args} />;
+
+export const AbInputQuantComponent = Template.bind({})
+AbInputQuantComponent.args = {
+    label: 'Uma label do InputQuant'
+} as AbInputQuantProps
